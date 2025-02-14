bbdad1f0b63ea478f284f82b7524e7ae426a5a00
yodatime <electriclikethor@gmail.com>
2025-02-14 11:14:26 +0200

    Add project files.

---
+++ GitHubTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitHubTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
